5b13ee586cff630d75b94bf13780b5367905ca94
Ruan-Klopper <231280@virtualwindow.co.za>
2024-10-17 19:15:34 +0200

    webconfig

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
