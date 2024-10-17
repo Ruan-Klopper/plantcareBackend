3404a17c88dea709ef408b14e974cd4b29efdcec
Ruan-Klopper <231280@virtualwindow.co.za>
2024-10-17 18:20:33 +0200

    main

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+// CORS setup for allowing requests from the frontend
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Your frontend URL, e.g., https://your-frontend-url.com
+    credentials: true,
+  })
+);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
+
+// API routes
+const userRoutes = require("./Routes/UserRoutes");
+app.use("/api/users", userRoutes);
+const productRoutes = require("./Routes/ProductRoutes");
+app.use("/api/products", productRoutes);
+const cartRoutes = require("./Routes/cartRoutes");
+app.use("/api/carts", cartRoutes);
+const plantsRoutes = require("./Routes/PlantsRoutes");
+app.use("/api/plants", plantsRoutes);
+const appointmentRoutes = require("./Routes/AppointmentRoutes");
+app.use("/api/appointments", appointmentRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
